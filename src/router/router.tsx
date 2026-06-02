01547302098213bc8a29088afbc78d938fa9ea13
9WiSHao <825763548@qq.com>
2026-06-02 17:07:47 +0800

    feat: github action流水线自动部署

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,13 +2,18 @@ import { createBrowserRouter } from 'react-router';
 import Home from '@/pages/Home/Home';
 import WarriorsDonk from '@/pages/15warriorsDonk/15warriorsDonk';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <Home />,
+        },
+        {
+            path: '/15warriorsDonk',
+            element: <WarriorsDonk />,
+        },
+    ],
     {
-        path: '/',
-        element: <Home />,
-    },
-    {
-        path: '/15warriorsDonk',
-        element: <WarriorsDonk />,
-    },
-]);
+        basename: import.meta.env.BASE_URL,
+    }
+);
