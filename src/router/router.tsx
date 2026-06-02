e5bc17760d9ad37c2ad8bd3e3c791493a879b42f
9WiSHao <825763548@qq.com>
2026-06-02 20:52:39 +0800

    fix: 刷新后重定向

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,14 @@ import { createBrowserRouter } from 'react-router';
 import Home from '@/pages/Home/Home';
 import WarriorsDonk from '@/pages/15warriorsDonk/15warriorsDonk';
 
+// 应对github page之类的重定向
+const redirect = sessionStorage.getItem('spa_redirect');
+if (redirect) {
+    sessionStorage.removeItem('spa_redirect');
+    const base = import.meta.env.BASE_URL;
+    window.history.replaceState(null, '', `${base}${redirect}`,);
+}
+
 export const router = createBrowserRouter(
     [
         {
