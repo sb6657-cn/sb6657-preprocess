5f90f50855f64c9c1c4029b717579d53eb09cd2a
9WiSHao <825763548@qq.com>
2026-06-02 20:42:56 +0800

    fix: githubpage对于spa跳转路径错误问题

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx'
 
+// 应对github page之类的重定向
+const redirect = sessionStorage.getItem('spa_redirect');
+if (redirect) {
+  sessionStorage.removeItem('spa_redirect');
+  const base = import.meta.env.BASE_URL;
+  window.history.replaceState(
+    null,
+    '',
+    `${base}${redirect}`,
+  );
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'node:path';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/sb6657-preprocess/',
+    base: '/sb6657-preprocess',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
