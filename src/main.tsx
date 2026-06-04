625d4144eda9782cd135c50c804fc2693e2b27c0
9WiSHao <825763548@qq.com>
2026-06-04 23:56:17 +0800

    feat: 右上角github角标

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+import GithubCorner from '@/components/GithubCorner/GithubCorner.tsx';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
+        <GithubCorner />
         <App />
     </StrictMode>
 );
