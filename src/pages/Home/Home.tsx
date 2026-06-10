366af45ad67ab298c7bcd5a4d6f356abe5e337ae
9WiSHao <825763548@qq.com>
2026-06-10 17:12:15 +0800

    feat: 首页点击跳转到主站

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,12 @@ import { Link } from 'react-router';
 import styles from './Home.module.scss';
 
 export default function Home() {
+    function handleIconClick() {
+        window.open('https://sb6657.cn', '_blank');
+    }
     return (
         <div className={styles.container}>
-            <div className={styles.title}>
+            <div className={styles.title} onClick={handleIconClick}>
                 <img className={styles.icon} src="https://apic.douyucdn.cn/upload/avatar_v3/201905/badbf01f7ab943358bf78bcd9245305f_big.jpg" alt="icon" />
                 <span className={styles.text}>sb6657 便捷工具集</span>
             </div>
